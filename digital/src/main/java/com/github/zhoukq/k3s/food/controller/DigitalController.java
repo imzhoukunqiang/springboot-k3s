78815343c3d0149f9c3a19affb9d2e23a4a34e7b
zhoukq <imzhoukunqiang@gmail.com>
2024-04-15 21:09:10 +0800

    1.0.0版本提交

--- digital/src/main/java/com/github/zhoukq/k3s/food/controller/DigitalController.java
+++ digital/src/main/java/com/github/zhoukq/k3s/food/controller/DigitalController.java
@@ -10,28 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/")
-public class HomeController {
+public class DigitalController {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger(DigitalController.class);
     public static final UniformRandomProvider RNG = RandomSource.XO_RO_SHI_RO_128_PP.create();
 
     @RequestMapping("/products")
-    public List<Product> foods() {
-
-
+    public List<Product> products() {
         ArrayList<Product> list = new ArrayList<>();
-        list.add(new Product("百事可乐", 3, RNG.nextInt(5, 500)));
-        list.add(new Product("风干牛肉", 49.9, RNG.nextInt(3, 300)));
-        list.add(new Product("去骨鸡爪", 38.9, RNG.nextInt(1, 300)));
-        list.add(new Product("乐事薯片", 7.8, RNG.nextInt(1, 300)));
-        list.add(new Product("恰恰瓜子", 12.5, RNG.nextInt(1, 300)));
-        list.add(new Product("雀巢咖啡", 32.9, RNG.nextInt(1, 300)));
+        list.add(new Product("iPhone 15", 5999, RNG.nextInt(5, 500)));
+        list.add(new Product("Xiaomi 14", 3799, RNG.nextInt(3, 300)));
+        list.add(new Product("Huawei Mate 60 Pro", 7999, RNG.nextInt(1, 300)));
         return list;
     }
-
 
 }
